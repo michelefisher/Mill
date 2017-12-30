d967db7ee5fcdffedd6cc6a100501c35efd15cc1
Dominik Trusinski <dominik.trusinski@gmail.com>
2017-12-30 13:32:22 +0100

    Add light shader. WIP: separate BasicObject into BasicColorObject and BasicTexturedObject

---
+++ objects/Light.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Dominik Trusinski on 30.12.2017.
+//
+
+#include "Light.h"
---
+++ objects/Light.h
@@ -0,0 +1,14 @@
+//
+// Created by Dominik Trusinski on 30.12.2017.
+//
+
+#ifndef MILL2_LIGHT_H
+#define MILL2_LIGHT_H
+
+
+class Light {
+
+};
+
+
+#endif //MILL2_LIGHT_H
